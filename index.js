16c36e969fbe7f20fb5b82815f74fa15b5fc2755
Mohammed Abbas <abbas.cloud21@gmail.com>
2022-09-30 22:37:22 -0400

    scripts

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://abbascloud21:abbascloud21@cluster0.b1qz6a6.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-app.set("port", PORT);
+app.use('/posts', postRoutes);
+app.use('/user', userRouter);
+app.get("/",(req,res)=>{
+res.send("Welcome to Memories API Backend!")
+})
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
   
-  app.use('/posts', postRoutes);
-  app.use('/user', userRouter);
+
 
 mongoose.set('useFindAndModify', false);
 
-app.get("/",(req,res)=>{
-  res.send("Welcome to Memories API Backend!")
-})
